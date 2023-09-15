e4211ac7b204023f61cfa1d0b6b128c4c2036601
222207054 <145006985+222207054@users.noreply.github.com>
2023-09-15 15:19:15 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,27 @@
-def isLeapYear(year):
-  if(year % 4 == 0 and year % 100 !=0) or year % 400 == 0 :
-    return True
-  else:
-    return False
-year=int(input(" Enter a year:")) 
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year)) 
-else:
-  print('{} is not a leap year.'.format(year)
+class BankAccount:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return f"Deposited ${amount}. New balance: ${self.balance}"
+        else:
+            return "Invalid deposit amount."
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self.balance}"
+        else:
+            return "Invalid withdrawal amount or insufficient funds."
+
+# Create an instance of BankAccount
+account = BankAccount()
+
+# Test deposit functionality
+print(account.deposit(100))  # Deposited $100. New balance: $100
+
+# Test withdrawal functionality
+print(account.withdraw(50))   # Withdrew $50. New balance: $50
+print(account.withdraw(75))   # Invalid withdrawal amount or insufficient funds
